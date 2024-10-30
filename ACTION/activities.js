bed81cab8753de4f3367b4ec784977f62cbb4576
JERRYst124 <24520590@gm.uit.edu.vn>
2024-10-30 23:12:58 +0700

    STRING

--- ACTION/activities.js
+++ ACTION/activities.js
@@ -1,1 +1,6 @@
 alert("HELLO FROM TROLL")
+let a = 5;
+let b = 5;
+
+console.log(a==b);
+console.log(a===b);
---
+++ STRINGTEMPLATE/STRINGTEMPLATE.js
@@ -0,0 +1,4 @@
+console.log("GA QUA")
+let a = 5;
+let b = 9;
+console.log(`ga is ${a+b} heeh`)
